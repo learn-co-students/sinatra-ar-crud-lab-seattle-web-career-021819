c55b29601dd02816c98bc89b54b23a23e074f145
Kimberly Heidorn <heidorkn@gmail.com>
2019-03-13 11:46:48 -0700

    built out interactive webpage using CRUD

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,42 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    redirect '/articles'
+  end
+
+  get '/articles/new' do
+    @article = Article.new
+    erb :new
+  end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  post '/articles' do
+    @article = Article.create(title: params[:title], content: params[:content])
+    redirect "/articles/#{@article.id}"
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    article = Article.find(params[:id])
+    @article = article.update(title: params[:title], content: params[:content])
+    redirect "/articles/#{@article.id}"
+  end
+
+  delete '/articles/:id' do
+    Article.delete(params[:id])
+    redirect '/'
   end
 end
